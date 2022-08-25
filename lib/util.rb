c7c029c3eadcf131970ed256a34845a5012d0547
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-08-25 15:57:38 -0500

    Add comment to swap function and made it private
    - Since it's a helper function is proper to have it private

--- lib/util.rb
+++ lib/util.rb
@@ -1,5 +1,17 @@
 class QuickSort
-  def self.swap(pair_array, start, end_index)
+
+  private
+
+  # This function swaps elements in a pair array to arrange all elements that are greater to the left and
+  # all elements that are smaller to the right.
+  #
+  # Starts by initializing an empty swap index which will be used to re-arrange elements while iterating over
+  # the pair array. When the first smaller element is found, it gets initialized to that element. If no smaller element is found, it will keep
+  # being nil and no swap will occur.
+  #
+  # The overall time complexity for this part of the algorithm is O(n), depending on the size of the portion of the array
+  # located in between start and end_index
+  def swap(pair_array, start, end_index)
     swap_index = nil
     pivot = end_index
     number_of_cycles = 0
