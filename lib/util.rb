ac2735dc8b868503a794a0eaf81217d32c2354c5
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-08-25 15:39:30 -0500

    Define QuickSort class to sort elements in an array
    - Define swap method to swap elements in an array based on a pivot. This
      is a helper method part of the QuickSort algorithm

--- lib/util.rb
+++ lib/util.rb
@@ -1,3 +1,36 @@
+class QuickSort
+  def self.swap(pair_array, start, end_index)
+    swap_index = nil
+    pivot = end_index
+    number_of_cycles = 0
+
+    (start...end_index).each do |index|
+      if pair_array[index][1] >= pair_array[pivot][1]
+        unless swap_index.nil?
+          temp = pair_array[index]
+          pair_array[index] = pair_array[swap_index]
+          pair_array[swap_index] = temp
+          swap_index += 1
+        end
+      elsif swap_index.nil?
+        swap_index = index
+      end
+
+      number_of_cycles += 1
+    end
+
+    unless swap_index.nil?
+      temp = pair_array[pivot]
+      pair_array[pivot] = pair_array[swap_index]
+      pair_array[swap_index] = temp
+      pivot = swap_index
+    end
+
+    puts "The number of cycles to swap elements during this call is: #{number_of_cycles}"
+    pivot
+  end
+end
+
 class Text
   def self.generate_word_count(text)
     # Starts by generating an array of words splited by separators using a regex
@@ -11,9 +44,7 @@ class Text
     arr_of_words.each do |word|
       number_of_cycles += 1
 
-      if words_counted[word] === nil
-        words_counted[word] = 0
-      end
+      words_counted[word] = 0 if words_counted[word].nil?
       words_counted[word] += 1
     end
 
@@ -22,4 +53,3 @@ class Text
     words_counted
   end
 end
-
