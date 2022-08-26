fcf288efb9b916b1b3875b28f98c3b7687330605
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-08-26 15:26:54 -0500

    Add much better styling to the web application
    - Create an styled component for the file upload using the label
    - Define JavaScript functionality to display the uploaded filename
    - Add some logic to the chartkick graph to set the height based on the
      length of the input
    - Define transitions and a much more elegant color scheme

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,13 @@
 // Entry point for the build script in your package.json
 import "chartkick/chart.js"
+
+fileField = document.querySelector('#file');
+fileSelected = document.querySelector('#file-selected');
+
+fileField.addEventListener('change', (e) => {
+  if (e.target.files[0]) {
+    fileSelected.textContent = `Selected file: ${e.target.files[0].name}`;
+  } else {
+    fileSelected.textContent = "No file has been uploaded"
+  }
+});
