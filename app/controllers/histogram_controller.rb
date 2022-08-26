92532a5ae49272a7ccb3b153a9e0f74e6cbbdd65
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-08-26 11:27:29 -0500

    Modify logic to use index view
    - Create locals variable and checks for existence before trying to
      create a chart

--- app/controllers/histogram_controller.rb
+++ app/controllers/histogram_controller.rb
@@ -1,6 +1,5 @@
 class HistogramController < ApplicationController
   def index
-    path = "public/some.txt"
-    @data = Text.generate_word_count(File.read(path))
+    render :index, locals: { data: [] }
   end
 end
